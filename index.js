11e9493c1c02026b6a873b2eec7067dd9394f1c2
Denis Ustinov <denchikden1721@gmail.com>
2022-11-23 21:57:06 +0500

    unzipping

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import admZip from "adm-zip";
 import path from 'path';
-import { fileURLToPath } from 'url';
-import {getZip} from './src/get.zip.js'
+import {fileURLToPath} from 'url';
+import {getZip} from './src/get.zip.js';
 
 getZip()
+
+const zip = new admZip("./files/data.zip")
+zip.extractAllTo("./files/", true)
 
