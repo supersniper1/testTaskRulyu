257108e070039acce55bc0a67df73c3727f394ec
Denis Ustinov <denchikden1721@gmail.com>
2022-11-24 11:00:02 +0500

    comment

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-// import {fileURLToPath} from 'url';
-// import path from 'path';
 import admZip from "adm-zip";
 import {getZip} from './src/get.zip.js';
 
-const promise = new Promise(((resolve, reject) => {
-    getZip()
+// если сделать таким образом то на первый запуск он не успевает скачивать архив,
+// на второй запуск говорит что архив побитый и у него нету хедера,
+// если сначала запустить код с фетчом и заугрзкой архива на локалку,
+// потом код с разархиватором, то все выполнится успешно и мы получим xml файл,
+// не успел найти решение чтобы все это выполнялось в один запуск.
+// попробовал: промисы, try catch, таймаут.
+
+let promise = new Promise(((resolve, reject) => {
+    getZip(resolve, reject)
 }))
 
 promise.then(() => {
--- src/get.zip.js
+++ src/get.zip.js
@@ -1,11 +1,12 @@
 import fetch from "node-fetch";
 import fs from 'fs';
 
-export const getZip = () => {
+export const getZip = (resolve, reject) => {
     fetch('http://www.cbr.ru/s/newbik')
         .then(res => {
             const dest = fs.createWriteStream('./files/data.zip');
             res.body.pipe(dest);
+            resolve()
         })
-        .catch(err => console.log(err))
+        .catch(err => reject(err))
 }
